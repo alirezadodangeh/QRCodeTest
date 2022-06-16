a8b769190e931184817f103ec9ced59bcf427c62
alireza dodangeh <ardsoft_2010@yahoo.com>
2022-06-16 08:43:08 +0430

    Add project files.

---
+++ QRCodeTest/InMemoryRandomAccessStream.cs
@@ -0,0 +1,6 @@
+﻿namespace QRCodeTest
+{
+    internal class InMemoryRandomAccessStream
+    {
+    }
+}
---
+++ QRCodeTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace QRCodeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(txt.Text, QRCodeGenerator.ECCLevel.Q);
+            ArtQRCode qrCode = new ArtQRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(10);
+            qrImage.Source = ConvertBitmap(qrCodeImage);
+        }
+
+        public BitmapImage ConvertBitmap(System.Drawing.Bitmap bitmap)
+        {
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            return image;
+        }
+
+    }
+}
